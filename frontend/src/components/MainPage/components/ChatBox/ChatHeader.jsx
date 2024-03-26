a7d7eb797292bb9a1dced173b2c914c1f69824fa
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-03-26 22:50:41 +0300

    deleted Header components

--- frontend/src/components/MainPage/components/ChatBox/ChatHeader.jsx
+++
@@ -1,8 +0,0 @@
-const CommentsField = () => (
-  <div className="bg-light mb-4 p-3 shadow-sm small">
-    <p className="m-0"><b># general</b></p>
-    <span className="text-muted">1 сообщение</span>
-  </div>
-);
-
-export default CommentsField;
