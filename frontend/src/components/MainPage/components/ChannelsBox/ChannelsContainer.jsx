5f2e552c9ac5509d40fedd7badd636db7147a569
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-03-26 23:17:47 +0300

    Deleted headers

--- frontend/src/components/MainPage/components/ChannelsBox/ChannelsContainer.jsx
+++ frontend/src/components/MainPage/components/ChannelsBox/ChannelsContainer.jsx
@@ -1,11 +1,34 @@
-import ChannelsHead from './ChannelsHead';
-import ChannelList from './ChannelList';
+import { useDispatch } from 'react-redux';
+import { Button } from 'react-bootstrap';
 
-const ChannelsContaner = () => (
-  <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-    <ChannelsHead />
-    <ChannelList />
-  </div>
-);
+import { actions as usActions } from '../../../../slices/ui';
+import ChannelList from './ChannelList';
+import addChannel from '../../../../assets/add_channel.svg';
+
+const ChannelsContaner = () => {
+  const dispatch = useDispatch();
+
+  const handleAddNewChannel = () => {
+    dispatch(usActions.openModal({ type: 'addChannel' }));
+  };
+
+  return (
+    <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+      <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+        <b>Каналы</b>
+        <Button
+          type="button"
+          variant="group-vertical"
+          className="p-0 text-primary"
+          onClick={handleAddNewChannel}
+        >
+          <img src={addChannel} alt="Добавить канал" />
+          <span className="visually-hidden">Добавить канал</span>
+        </Button>
+      </div>
+      <ChannelList />
+    </div>
+  );
+};
 
 export default ChannelsContaner;
--- frontend/src/components/MainPage/components/ChatBox/ChatContainer.jsx
+++ frontend/src/components/MainPage/components/ChatBox/ChatContainer.jsx
@@ -1,18 +1,31 @@
 import { useSelector } from 'react-redux';
 import { selectChannels } from '../../../../slices/channelsSlice';
-import ChatHeader from './ChatHeader';
 import CommentsField from './CommentsField';
 import InputField from './InputField';
 
 const ChatContainer = () => {
   const channels = useSelector(selectChannels);
   const activeId = useSelector((state) => state.ui.activeChannelId);
+  const messages = useSelector((state) => state.messages.entities);
+
   const channel = channels.find(({ id }) => id === activeId);
+  const activeChannelMesages = Object
+    .values(messages)
+    .filter(({ channelId }) => channelId === activeId);
 
   return (
     <div className="col p-0 h-100">
       <div className="d-flex flex-column h-100">
-        <ChatHeader />
+        <div className="bg-light mb-4 p-3 shadow-sm small">
+          <p className="m-0">
+            <b>
+              {`# ${channel?.name}`}
+            </b>
+          </p>
+          <span className="text-muted">
+            {`${activeChannelMesages.length} сообщение`}
+          </span>
+        </div>
         <CommentsField />
         <InputField channel={channel} />
       </div>
