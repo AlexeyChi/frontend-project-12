6bc1ad7af78e0587f449ac9016d6759777a0e079
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-04-02 18:59:23 +0300

    added deletPath

--- frontend/src/routes/apiRoutes.js
+++ frontend/src/routes/apiRoutes.js
@@ -4,5 +4,6 @@ export default {
   singupPath: () => [apiPath, 'signup'].join('/'),
   loginPath: () => [apiPath, 'login'].join('/'),
   channelsPath: () => [apiPath, 'channels'].join('/'),
+  deletedChannel: (id) => [apiPath, 'channels', id].join('/'),
   messagesPath: () => [apiPath, 'messages'].join('/'),
 };
