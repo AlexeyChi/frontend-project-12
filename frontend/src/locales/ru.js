93163bb399ed0061566beeef643714e9828da82c
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-04-04 01:08:09 +0300

    added default locale

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,45 @@
+export default {
+  translation: {
+    navBar: {
+      logoutBtn: 'Выйти',
+    },
+    login: {
+      header: 'Войти',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      loginBtn: 'Войти',
+      newUser: 'Нет аккаунта?',
+      signupLink: 'Регистрация',
+    },
+    signup: {
+      header: 'Регистрация',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      confirmPassword: 'Подтвердите пароль',
+      signupBtn: 'Зарегистрироваться',
+    },
+    channels: {
+      header: 'Каналы',
+      addBtn: 'Добавить',
+      delete: 'Удалить',
+      rename: 'Переименовать',
+    },
+    chat: {
+      messages: {
+        message_one: '{{count}} сообщение',
+        message_many: '{{count}} сообщений',
+        message_few: '{{count}} сообщения',
+      },
+      addMessage: 'Введите сообщение...',
+      sendBtn: 'Отправить',
+    },
+    errors: {
+      required: 'Обязательное поле',
+      usernameLength: 'От 3 до 20 символов',
+      shortPassword: 'Не менее 6 символов',
+      mustMatch: 'Пароль должен совпадать',
+      userAlreadyExist: 'Такой пользователь уже существует',
+      authFailed: 'Неверные имя пользователя или пароль',
+    },
+  },
+};
