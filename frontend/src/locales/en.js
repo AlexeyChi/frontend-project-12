dcbbb27d0c9a25d48d3a51387a77388e5be33863
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-04-04 14:08:03 +0300

    added locales

---
+++ frontend/src/locales/en.js
@@ -0,0 +1,57 @@
+export default {
+  translation: {
+    navBar: {
+      logoutBtn: 'Log out',
+    },
+    login: {
+      header: 'Log in',
+      username: 'Username',
+      password: 'Password',
+      loginBtn: 'LogIn',
+      newUser: 'No account?',
+      signupLink: 'Sign up',
+    },
+    signup: {
+      header: 'Registration',
+      username: 'Username',
+      password: 'Password',
+      confirmPassword: 'Confirm password',
+      signupBtn: 'Sign up',
+    },
+    channels: {
+      header: 'Channels',
+      addBtn: 'Add',
+      delete: 'Delete',
+      rename: 'Rename',
+    },
+    chat: {
+      messages: {
+        message_one: '{{count}} message',
+        message_many: '{{count}} messages',
+      },
+      addMessage: 'Enter new message...',
+      sendBtn: 'Send',
+      newMessage: 'Add message',
+    },
+    modals: {
+      addChannelHeader: 'Add channel',
+      enterChannelName: 'Enter channel name...',
+      removeChannelHeader: 'Remove channel',
+      areYouShure: 'Shure?',
+      renameChannelHeader: 'New channel name',
+      cancelBtn: 'Cancel',
+      sendBtn: 'Send',
+      addBtn: 'Add',
+      removeBtn: 'Remove',
+    },
+    errors: {
+      required: 'Required',
+      inValidLength: '3 or 20 characters',
+      notUniqueName: 'Must be unique',
+      shortPassword: 'Min 6 characters',
+      mustMatch: 'Password must match',
+      userAlreadyExist: 'User already taken',
+      authFailed: 'Username or password is incorrect',
+    },
+  },
+};
