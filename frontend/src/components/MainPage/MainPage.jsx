5359364439a32566783258f98725325f9787345c
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-04-05 22:49:53 +0300

    added fetchMessages

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { fetchChannels } from '../../slices/channelsSlice.js';
 
 import { useAuth } from '../../hooks/index.jsx';
+import { fetchChannels } from '../../slices/channelsSlice.js';
+import { fetchedMessages } from '../../slices/messagesSlise.js';
 import Modal from '../Modal/Modal.jsx';
 import ChannelsContaner from './components/ChannelsBox/ChannelsContainer.jsx';
 import ChatContainer from './components/ChatBox/ChatContainer.jsx';
@@ -14,6 +15,7 @@ const MainPage = () => {
   useEffect(() => {
     const headers = auth.getAuthHeaders();
     dispatch(fetchChannels(headers));
+    dispatch(fetchedMessages(headers));
   }, [dispatch, auth]);
 
   return (
