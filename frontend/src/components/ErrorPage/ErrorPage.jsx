7822d433ffeb3bccca017b57a028363296e09e44
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-04-05 22:38:41 +0300

    added link to main page

--- frontend/src/components/ErrorPage/ErrorPage.jsx
+++ frontend/src/components/ErrorPage/ErrorPage.jsx
@@ -1,17 +1,32 @@
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
+
 import PageTemplate from '../PageTemplate/PageTemplate';
+import routes from '../../routes';
 import img from '../../assets/pageNotFound.jpg';
 
-const ErrorPage = () => (
-  <PageTemplate>
-    <div className="d-flex justify-content-center align-content-center">
-      <Card className="shadow-sm align-items-center max-w-500">
-        <Card.Body className="row p-5">
-          <Card.Img variant="top" src={img} alt="Error 404: Page not found" />
-        </Card.Body>
-      </Card>
-    </div>
-  </PageTemplate>
-);
+const ErrorPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <PageTemplate>
+      <div className="d-flex justify-content-center align-content-center">
+        <Card className="shadow-sm align-items-center max-w-500">
+          <Card.Body className="row p-5">
+            <Card.Img variant="top" src={img} alt="Error 404: Page not found" />
+            <div className="card-footer p-4">
+              <div className="text-center">
+                {t('errorPage.pageNotFound')}
+                {' '}
+                <Link to={routes.app.mainPage()}>{t('errorPage.mainPage')}</Link>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </PageTemplate>
+  );
+};
 
 export default ErrorPage;
