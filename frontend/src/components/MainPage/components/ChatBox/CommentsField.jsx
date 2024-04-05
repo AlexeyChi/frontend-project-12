d680848524d62e101c81600905227bbf010c9fdc
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-04-05 22:45:15 +0300

    added scroll

--- frontend/src/components/MainPage/components/ChatBox/CommentsField.jsx
+++ frontend/src/components/MainPage/components/ChatBox/CommentsField.jsx
@@ -1,16 +1,23 @@
+import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 const CommentsField = () => {
   const activeId = useSelector((state) => state.ui.activeChannelId);
   const getMessages = useSelector((state) => state.messages);
   const messages = Object
-    .values(getMessages?.entities)
+    .values(getMessages.entities)
     .filter(({ channelId }) => channelId === activeId);
+
+  const srcollRef = useRef(null);
+
+  useEffect(() => {
+    srcollRef.current?.scrollIntoView({ behavior: 'smooth' });
+  });
 
   return (
     <div id="messages-box" className="chat-messages overflow-auto px-5">
       {messages.map(({ id, username, body }) => (
-        <div key={id} className="text-break mb-2">
+        <div key={id} className="text-break mb-2" ref={srcollRef}>
           <b>{username}</b>
           {': '}
           {body}
