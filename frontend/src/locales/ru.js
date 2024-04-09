06b7adfb10cf7bac60d300f146292ce4316777cc
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-04-09 16:56:19 +0300

    fixed error text

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -57,7 +57,7 @@ export default {
       inValidLength: 'От 3 до 20 символов',
       notUniqueName: 'Должно быть уникальным',
       shortPassword: 'Не менее 6 символов',
-      mustMatch: 'Пароль должен совпадать',
+      mustMatch: 'Пароли должны совпадать',
       userAlreadyExist: 'Такой пользователь уже существует',
       authFailed: 'Неверные имя пользователя или пароль',
       network: 'Ошибка соединения',
